159268b0b1a48eaa9dcbe6ed59dd02ed41bf2265
Strawberryai <ag6154lk@gmail.com>
2023-10-29 19:59:18 +0100

    Scatter hecho

--- proyecto/main.py
+++ proyecto/main.py
@@ -5,7 +5,10 @@ from hierarchical_clustering import hierarchical_clustering
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
 
 # VARIABLES GLOBALES
 data_dir = "."
@@ -49,8 +52,13 @@ def load_hc_model(filename):
 # MAIN
 def main():
     train = pd.read_csv(train_file)
+    # Realizamos un encoding de las clases de las instancias: Pneumonia -> 0; Stroke -> 1...
+    le = LabelEncoder()
+    train['Clases'] = le.fit_transform(train['gs_text34']) 
+
+    
     x_train = train['open_response']
-    y_train = train['gs_text34']
+    y_train = train['Clases']
     
     print(x_train.head())
     print(y_train.head())
@@ -70,28 +78,47 @@ def main():
     #proc.draw_dendrogram()
     
     # Cortar el arbol en un numero determinado de clusters
-    proc.cortar_arbol(num_clusters=3,dist_max=0)
+    proc.cortar_arbol(num_clusters=10,dist_max=0)
     
     # Obtener labels por cada vector -> diccionario
     labels = proc.predict_multiple(vectores)
     #print(labels)
 
-    # Reducir las dimensiones para visualizarlas: PCA
-    pca = PCA(n_components=2)
+    ## Reducir las dimensiones para visualizarlas: PCA -> 2D
+    #pca = PCA(n_components=2)
+    #pca.fit(vectores)
+    ## Cambio de base a dos dimensiones PCA 
+    #x_train_PCAspace = pca.transform(vectores)
+    #print('Dimensiones después de aplicar PCA: ')
+    #samples = 300 # Número de instancias a dibujar
+    ## Dibujar los puntos en el espacio, color: cluster, etiqueta-numérica: clase
+    ## Color del punto: cluster
+    #sc = plt.scatter(x_train_PCAspace[:samples,0],x_train_PCAspace[:samples,1], cmap=plt.cm.get_cmap('nipy_spectral', 10),c=list(labels.values())[:samples])
+    #plt.colorbar()
+#
+    ## Etiqueta numérica: clase 
+    #for i in range(samples):
+    #    plt.text(x_train_PCAspace[i,0],x_train_PCAspace[i,1], y_train[i])
+    #plt.show()
+    
+    
+    # Reducir las dimensiones para visualizarlas: PCA -> 2D
+    pca = PCA(n_components=3)
     pca.fit(vectores)
     # Cambio de base a dos dimensiones PCA 
     x_train_PCAspace = pca.transform(vectores)
-    print('Dimensiones después de aplicar PCA: ',x_train_PCAspace.shape)  
-    samples = 10 # Número de instancias a dibujar
+    print('Dimensiones después de aplicar PCA: ')
+    samples = 300 # Número de instancias a dibujar
     # Dibujar los puntos en el espacio, color: cluster, etiqueta-numérica: clase
     # Color del punto: cluster
-    sc = plt.scatter(x_train_PCAspace[:samples,0],x_train_PCAspace[:samples,1], cmap=plt.cm.get_cmap('nipy_spectral', 10),c=list(labels.values())[:samples])
-    plt.colorbar()
-
-    # Etiqueta numérica: clase 
-    for i in range(samples):
-        plt.text(x_train_PCAspace[i,0],x_train_PCAspace[i,1], y_train[i])
-
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x_train_PCAspace[:samples,0],x_train_PCAspace[:samples,1], x_train_PCAspace[:samples,2], cmap=plt.cm.get_cmap('nipy_spectral', 10),c=list(labels.values())[:samples])
+    ax.set_xlabel('Eje X')
+    ax.set_xlabel('Eje Y')
+    ax.set_xlabel('Eje Z')
+    plt.show()
+    
 if __name__ == "__main__":
     main()
     exit(0)
